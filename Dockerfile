b124ad281b003ce1567e6a687fc966a0fe47ecdd
Grigory Borisenko <7010504@gmail.com>
2022-12-26 13:45:14 +0300

    -m param

--- Dockerfile
+++ Dockerfile
@@ -7,14 +7,13 @@ COPY jupyterhub_config.py jupyterhub_config.py
 #ENTRYPOINT [ "entrypoint.sh" ]
 
 #COPY $NOTEBOOKS_FROM /home$NOTEBOOKS_FROM
-
 ARG USER=admin
 ARG PASSWORD=admin
 RUN pip install notebook
 
-RUN useradd ${USER} -p $(openssl passwd ${PASSWORD})
+RUN useradd ${USER} -p $(openssl passwd ${PASSWORD}) -m
 VOLUME /home
-EXPOSE 80
+EXPOSE 8000
 CMD ["jupyterhub"]
 
 
--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -3,10 +3,11 @@ c = get_config()
 c.JupyterHub.authenticator_class = 'jupyterhub.auth.PAMAuthenticator'
 c.Authenticator.allowed_users = {'admin'}
 c.Authenticator.admin_users = {'admin'}
-def hook(sp):
-    name = sp.user.name
-    try:
-        pwd.getpwnam(name)
-    except KeyError:
-        subprocess.check_call(['useradd', '-ms', '/bin/bash', name])
-c.Spawner.pre_spawn_hook = hook
+
+#def hook(sp):
+ #   name = sp.user.name
+  #  try:
+   #     pwd.getpwnam(name)
+    #except KeyError:
+     #   subprocess.check_call(['useradd', '-ms', '/bin/bash', name])
+#c.Spawner.pre_spawn_hook = hook
